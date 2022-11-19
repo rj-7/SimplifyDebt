ba9c537fc710af466b1e54e2b5797609bb9a85fc
Rakesh Chowdary Yarlagadda <yrakeshchowdary1116@gmail.com>
2022-11-18 17:05:15 -0700

    added authentication feature

--- loginscreen/src/main/java/com/example/loginscreen/LoginFragment.kt
+++ loginscreen/src/main/java/com/example/loginscreen/LoginFragment.kt
@@ -1,12 +1,16 @@
 package com.example.loginscreen
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.loginscreen.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 internal class LoginFragment : Fragment() {
 
@@ -16,6 +20,7 @@ internal class LoginFragment : Fragment() {
         }
     }
 
+    private lateinit var firebaseAuth: FirebaseAuth
     private var _binding: FragmentLoginBinding? = null
     private val binding: FragmentLoginBinding
         get() = _binding!!
@@ -27,6 +32,34 @@ internal class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
+       // setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.signLinkText.setOnClickListener {
+            val intent = Intent(activity, RegisterFragment::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.loginUserName.text.toString()
+            val pass = binding.password.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(activity, AuthActivity::class.java)
+                        startActivity(intent)
+                    } else {
+
+                        Toast.makeText(activity, "Please enter correct details", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(activity, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
         return binding.root
     }
 
--- loginscreen/src/main/java/com/example/loginscreen/RegisterFragment.kt
+++ loginscreen/src/main/java/com/example/loginscreen/RegisterFragment.kt
@@ -1,12 +1,15 @@
 package com.example.loginscreen
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.loginscreen.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterFragment : Fragment() {
     companion object {
@@ -16,6 +19,7 @@ class RegisterFragment : Fragment() {
     }
 
     private var _binding: FragmentRegisterBinding? = null
+    private lateinit var firebaseAuth: FirebaseAuth
     private val binding: FragmentRegisterBinding
         get() = _binding!!
     val viewModel: AuthViewModel by viewModels<AuthViewModel> {
@@ -27,6 +31,38 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.signLinkText.setOnClickListener {
+            val intent = Intent(activity, LoginFragment::class.java)
+            startActivity(intent)
+        }
+        binding.signUpButton.setOnClickListener {
+            val email = binding.loginEmail.text.toString()
+            val pass = binding.password.text.toString()
+            val confirmPass = binding.confirmPassword.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (pass == confirmPass) {
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(activity, LoginFragment::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(activity, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+                } else {
+                    Toast.makeText(activity, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(activity, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
         return binding.root
     }
 
