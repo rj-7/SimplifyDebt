1bd074fe917d348efd02e89053bfff74792679ad
Rakesh Chowdary Yarlagadda <yrakeshchowdary1116@gmail.com>
2022-12-04 00:04:37 -0700

    added create group

--- dashboardscreen/src/main/java/com/example/dashboardscreen/CreateGroupActivity.kt
+++ dashboardscreen/src/main/java/com/example/dashboardscreen/CreateGroupActivity.kt
@@ -3,28 +3,16 @@ package com.example.dashboardscreen
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
-import kotlin.system.exitProcess
 
-class AddExpenseActivity: AppCompatActivity() {
+class CreateGroupActivity: AppCompatActivity() {
     lateinit var db: FirebaseFirestore
 
     companion object {
         fun getIntent(context: Context): Intent =
-            Intent(context, AddExpenseActivity::class.java).apply {
+            Intent(context, CreateGroupActivity::class.java).apply {
 
             }
     }
@@ -35,12 +23,12 @@ class AddExpenseActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_addexpense)
+        setContentView(R.layout.activity_create_group)
         //setSupportActionBar(findViewById(R.id.toolbar))
-      //  viewModel.getUserData(FirebaseAuth.getInstance().currentUser?.uid).observe(this) {
-       //     viewModel.userData = it
-            addFragment(AddExpenseFragment.getInstance())
-      //  }
+        //  viewModel.getUserData(FirebaseAuth.getInstance().currentUser?.uid).observe(this) {
+        //     viewModel.userData = it
+        addFragment(CreateGroupFragment.getInstance())
+        //  }
 //        viewModel.navigationLiveData.observe(this) {
 //            when (it) {
 //                is DashboardViewModel.DashboardNavigationEvent.GoToFriendDetails -> {
