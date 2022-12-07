dd3b28ef1726c6e948937b7ed4fa1c4212389475
Rakesh Chowdary Yarlagadda <yrakeshchowdary1116@gmail.com>
2022-12-07 16:21:25 -0700

    totalBalance changes

--- loginscreen/build/generated/ap_generated_sources/debug/out/com/example/dashboardscreen/BR.java
+++
@@ -1,5 +0,0 @@
-package com.example.dashboardscreen;
-
-public class BR {
-  public static int _all = 0;
-}
--- loginscreen/build/generated/data_binding_trigger/debug/com/example/loginscreen/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.loginscreen;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
